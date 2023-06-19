b17bd8f17bf3baa35e706f34fab78f51d3bb4e4d
Enes <nsyagz@gmail.com>
2023-06-19 15:13:19 +0300

    Product added, if route product/add cannot see categories

--- campproject/src/Layouts/Dashboard.jsx
+++ campproject/src/Layouts/Dashboard.jsx
@@ -2,29 +2,38 @@ import React from 'react';
 import Categories from './Categories';
 import ProductList from '../Pages/ProductList';
 import { Grid } from 'semantic-ui-react';
-import { Route, Routes } from "react-router";
+import { Route, Routes, useLocation } from "react-router";
 import ProductDetail from '../Pages/ProductDetail';
 import CartDetail from '../Pages/CartDetail';
 import { ToastContainer } from 'react-toastify';
 import Error404 from '../Pages/Error404';
+import ProductAdd from '../Pages/ProductAdd';
 
 export default function Dashboard() {
+    const location = useLocation();
+
+    // Eğer "/product/add" yoluna gitmişse, categoriesShown değişkenini false olarak ayarla
+    const categoriesShown = location.pathname !== '/product/add';
+
     return (
         <div>
-            <ToastContainer position='bottom-right'/>
+            <ToastContainer position='bottom-right' />
             <Grid>
                 <Grid.Row>
+                {categoriesShown && (
                     <Grid.Column width={4}>
                         <Categories />
                     </Grid.Column>
+                    )}
                     <Grid.Column width={12}>
                         <Routes>
                             <Route exact path="/" element={<ProductList />} />
                             <Route path="/products" element={<ProductList />} />
                             <Route path="/products/:id" element={<ProductDetail />} />
                             <Route path="/cart" element={<CartDetail />} />
-                            <Route path="*" element={<Error404/>} />
-                            <Route path="/404" element={<Error404/>} />
+                            <Route path="/product/add" element={<ProductAdd />} />
+                            <Route path="*" element={<Error404 />} />
+                            <Route path="/404" element={<Error404 />} />
 
                         </Routes>
                     </Grid.Column>
--- campproject/src/Pages/ProductAdd.jsx
+++ campproject/src/Pages/ProductAdd.jsx
@@ -1,12 +1,86 @@
 import React from 'react'
-import { Formik } from 'formik'
+import '../assets/Dashboard.css';
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+import * as Yup from "yup" // birden fazla fonksiyon dahil edilmiş Yup. ile erişim sağlanıyor
+import { FormField, Button, Label } from "semantic-ui-react"
 
 export default function ProductAdd() {
+  const initalValues = { productName: "", unitPrice: 10, unitsInStock: 5, quantityPerUnit: "", productImage: "", categoryName: "none" }
+  const schema = Yup.object({
+    productName: Yup.string().required("Ürün adı zorunlu"), // metinsel ve zorunlu alan
+    unitPrice: Yup.number().required("Ürün fiyatı zorunlu"),
+    unitsInStock: Yup.number().required("Ürün stok sayısı zorunlu"),
+    quantityPerUnit: Yup.string().required("Ürün stok durumu zorunlu"),
+    categoryName: Yup.string().required("Ürün kategori ismi zorunlu"),
+    productImage: Yup.string().required("Ürün resimi zorunlu")
+  })
+
   return (
-    <div>
-        <Formik>
-            
-        </Formik>
-    </div>
+
+    <Formik
+      initialValues={initalValues}
+      validationSchema={schema}
+      onSubmit={(values) => {
+        console.log(values)
+      }}
+    >
+      <div className="container">
+        <div className="card">
+          <Form className="ui form">
+            <FormField>
+              <Field name="productName" placeholder="Ürün adı" />
+              <ErrorMessage name="productName" render={error =>
+                <Label pointing basic color="red" content={error}></Label>
+              }></ErrorMessage>
+            </FormField>
+            <FormField>
+              <Field name="unitPrice" placeholder="Ürün fiyatı" />
+              <ErrorMessage name="unitPrice" render={error =>
+                <Label pointing basic color="red" content={error}></Label>
+              }></ErrorMessage>
+            </FormField>
+            <FormField>
+              <Field name="unitsInStock" placeholder="Ürün stoğu" />
+              <ErrorMessage name="unitsInStock" render={error =>
+                <Label pointing basic color="red" content={error}></Label>
+              }></ErrorMessage>
+            </FormField>
+            <FormField>
+              <Field name="quantityPerUnit" placeholder="Ürün durumu" />
+              <ErrorMessage name="quantityPerUnit" render={error =>
+                <Label pointing basic color="red" content={error}></Label>
+              }></ErrorMessage>
+            </FormField>
+            <FormField>
+              <Field name="productImage" placeholder="Ürün resimi" />
+              <ErrorMessage name="productImage" render={error =>
+                <Label pointing basic color="red" content={error}></Label>
+              }></ErrorMessage>
+            </FormField>
+            {/* <FormField>
+              <input
+                type="file"
+                id="productImage"
+                name="productImage"
+                onChange={(event) => {
+                  const file = event.currentTarget.files[0];
+                  formik.setFieldValue('productImage', file);
+                }}
+              />
+            </FormField> */}
+            <FormField>
+              <Field name="categoryName" placeholder="Ürün kategorisi" />
+              <ErrorMessage name="categoryName" render={error =>
+                <Label pointing basic color="red" content={error}></Label>
+              }></ErrorMessage>
+            </FormField>
+            <Button color="green" type="submit">
+              Ekle
+            </Button>
+          </Form>
+        </div>
+      </div>
+    </Formik>
+
   )
 }
